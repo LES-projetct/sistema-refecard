8e9d33c0309e2090e5d265277ea6facd26d9ac6f
Taylormg <taylormarchioreg@gmail.com>
2026-03-30 23:32:08 -0300

    mudança simples para fluxo de usuario

--- src/main/java/com/refecard/sistema/controller/UsuarioController.java
+++ src/main/java/com/refecard/sistema/controller/UsuarioController.java
@@ -6,15 +6,27 @@ import org.springframework.web.bind.annotation.*;
 import com.refecard.sistema.model.usuario.Usuario;
 import com.refecard.sistema.service.UsuarioService;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioController {
 
     @Autowired
-    private UsuarioService usuarioService;
+    private UsuarioService service;
 
-    @PostMapping("/teste")
-    public Usuario criarUsuarioTeste() {
-        return usuarioService.criarUsuarioTeste();
+    @PostMapping
+    public Usuario criar(@RequestBody Usuario usuario) {
+        return service.salvar(usuario);
+    }
+
+    @GetMapping
+    public List<Usuario> listar() {
+        return service.listar();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id) {
+        service.deletar(id);
     }
 }
--- src/main/java/com/refecard/sistema/service/UsuarioService.java
+++ src/main/java/com/refecard/sistema/service/UsuarioService.java
@@ -4,35 +4,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.refecard.sistema.model.usuario.Usuario;
-import com.refecard.sistema.model.usuario.UsuarioRole;
-import com.refecard.sistema.model.usuario.Role;
 import com.refecard.sistema.repository.UsuarioRepository;
 
-import lombok.RequiredArgsConstructor;
+import java.util.List;
 
 @Service
-//@RequiredArgsConstructor // Gera um construtor com argumentos para todos os campos
 public class UsuarioService {
 
     @Autowired
-    private UsuarioRepository usuarioRepository;
+    private UsuarioRepository repository;
 
-    public Usuario criarUsuarioTeste() {
+    public Usuario salvar(Usuario usuario) {
+        return repository.save(usuario);
+    }
 
-        Usuario usuario = new Usuario();
-        usuario.setNome("João Teste");
-        usuario.setEmail("joao.teste@email.com");
-        usuario.setCpf("12345678900");
-        usuario.setSenha("123456");
-        usuario.setDataNascimento(java.time.LocalDate.of(2000, 1, 1));
+    public List<Usuario> listar() {
+        return repository.findAll();
+    }
 
-        // Criando ROLE
-        UsuarioRole role = new UsuarioRole();
-        role.setNomeRole(Role.CLIENTE);
-        role.setUsuario(usuario);
-
-        usuario.getRoles().add(role);
-
-        return usuarioRepository.save(usuario);
+    public void deletar(Long id) {
+        repository.deleteById(id);
     }
 }
